f6a2bfd9e5745009ebf08be4510dd9676a25da45
selrahcsan <charlesstna@proton.me>
2025-09-23 14:48:24 -0300

    Solução desafio 03: Conversor de Unidades (Metros para Centímetros)

--- 01 - Nível Iniciante/desafio03.py
+++ 01 - Nível Iniciante/desafio03.py
@@ -1,1 +1,13 @@
+# Conversor de Unidades (Metros para Centímetros): Peça ao usuário um valor em metros e converta-o para centímetros.
+
+def converter(num):
+    return num * 100
+
+def main():
+    num = int(input('Digite o valor em cm: '))
+    resultado = converter(num)
+    print(f'{num}m é igual a {resultado}cm')
+
+if __name__ == '__main__':
+    main()
 
