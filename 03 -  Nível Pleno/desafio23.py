c4ea5c6648dfaff6cbd5d53473173260b9663585
selrahcsan <charlesstna@proton.me>
2025-10-16 10:02:33 -0300

    Solucionado o desafio  17: List Comprehension para Múltiplos

---
+++ 03 -  Nível Pleno/desafio23.py
@@ -0,0 +1,12 @@
+#  **List Comprehension para Múltiplos:** Use list comprehension para criar uma lista de todos os números entre 1 e 100 que são divisíveis por 7.
+
+def listar():
+    num = [i for i in range(1,101) if i % 7 == 0]
+    return num
+
+def main():
+    print('Lista de Número de 1 a 100, que são divisíveis por 7!')
+    print(listar())
+
+if __name__ == '__main__':
+    main()
