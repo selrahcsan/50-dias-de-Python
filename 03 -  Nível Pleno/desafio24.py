9438990dd0a83b8c6b61bc0938c7f1ff989443ef
selrahcsan <charlesstna@proton.me>
2025-10-16 12:39:46 -0300

    Solucionado o desafio 24: Calculadora com Funções

--- 03 -  Nível Pleno/desafio24.py
+++ 03 -  Nível Pleno/desafio24.py
@@ -1,4 +1,4 @@
-#  **Operações Matemáticas:** Peça dois números e exiba o resultado da soma, subtração, multiplicação e divisão.
+#  **Calculadora com Funções:** Refatore a calculadora do nível iniciante usando funções separadas para cada operação (soma, subtração, etc.).
 
 def soma(num1, num2):
     return num1 + num2
@@ -10,11 +10,8 @@ def multiplicacao(num1, num2):
     return num1 * num2
 
 def divisao(num1, num2):
-    if num2 == 0:
-        return 'Divisão por zero'
-    else:
-        return num1 / num2
-
+    return num1 / num2 if num2 !=0 else 'Divisão por Zero'
+    
 def main():
     num1 = int(input('Qual o primeiro Número:\n'))
     num2 = int(input('Qual o segundo número:\n'))
